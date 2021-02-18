24c78d18220539d6b4808072b819925f2c2841f6
lehkhazirna <75725228+lehkhazirna@users.noreply.github.com>
2021-02-18 18:53:55 +0530

    Delete Linear Regression/Single Variable Linear Descent directory

--- Linear Regression/Single Variable Linear Descent/plotData.m
+++
@@ -1,8 +0,0 @@
-%===============Plot on a graph==================
-%================================================
-function plotData(x,y)
-  figure;
-  plot(x,y,'rx','MarkerSize',8);
-  xlabel("Training Set");
-  ylabel("Prediction results");
-end
